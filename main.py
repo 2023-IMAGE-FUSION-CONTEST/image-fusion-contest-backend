4e080959cc6821acb4fca83775af8e28ae9c2610
PRESSO <caff1nepill@gmail.com>
2023-07-01 18:32:41 +0900

    add image fusion api

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+import requests
+import json
+
+origins = [
+	"http://127.0.0.1:3000",
+	"api.presso.codes"
+]
+
+app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"]
+)
+
+
+class Item(BaseModel):
+	fn_index: int
+	data: list
+
+
+@app.post("/api/image-fusion")
+def post_image_fusion(item: Item):
+	url = "https://demo.70e9f1a0f18a49a29a0cd16c9e0750df.lambdaspaces.com/run/predict"
+	headers = {
+		"Content-Type": "application/json"
+	}
+	data = {
+		"fn_index": item.fn_index,
+		"data": item.data
+	}
+
+	response = requests.post(url, headers=headers, json=data)
+	return json.loads(response.text)
